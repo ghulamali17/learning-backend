5569f54aef7e493c180fad8b91e6ce822737dc95
ghulamali17 <ghulamali47092@gmail.com>
2025-07-13 00:23:51 +0500

    rest apis

--- express/rest-api/index.js
+++ express/rest-api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
 const port = 8000;
 
@@ -19,24 +20,41 @@ app.get("/users", (req, res) => {
 
 app.listen(port, () => console.log("Server start at port " + port));
 
-app.get("/api/users/:id", (req,res)=>{
- const id=Number(req.params.id)
- const user=users.find((user)=> user.id==id)
- return res.json(user)
-})
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id == id);
+  return res.json(user);
+});
 
-app.post("/api/users",(req,res)=>{
-  return res.send({status: "Pending"})
-})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.patch("/api/users/:id", (req,res)=>{
- const id=Number(req.params.id)
- const user=users.find((user)=> user.id==id)
-  return res.send({status: "Pending"})
-})
 
-app.delete("/api/users/:id", (req,res)=>{
- const id=Number(req.params.id)
- const user=users.find((user)=> user.id==id)
-  return res.send({status: "Pending"})
-})
+app.post("/api/users", (req, res) => {
+  const data = req.body;
+  const newUser = { ...data, id: users.length + 1 };
+
+  users.push(newUser);
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) {
+      console.error("Failed to write to file", err);
+      return res
+        .status(500)
+        .send({ status: "Error", message: "Could not save user" });
+    }
+    res.send({ status: "Success", user: newUser });
+  });
+});
+
+app.patch("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id == id);
+  return res.send({ status: "Pending" });
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id == id);
+  return res.send({ status: "Pending" });
+});
