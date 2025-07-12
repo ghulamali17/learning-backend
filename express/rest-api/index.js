7592881fad92f79b9258838409f7b052c91945c7
ghulamali17 <ghulamali47092@gmail.com>
2025-07-12 23:42:42 +0500

    rest-api

---
+++ express/rest-api/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+const port = 8000;
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+
+  res.send(html);
+});
+
+app.listen(port, () => console.log("Server start at port " + port));
+
+app.get("/api/users/:id", (req,res)=>{
+ const id=Number(req.params.id)
+ const user=users.find((user)=> user.id==id)
+ return res.json(user)
+})
+
+app.post("/api/users",(req,res)=>{
+  return res.send({status: "Pending"})
+})
+
+app.patch("/api/users/:id", (req,res)=>{
+ const id=Number(req.params.id)
+ const user=users.find((user)=> user.id==id)
+  return res.send({status: "Pending"})
+})
+
+app.delete("/api/users/:id", (req,res)=>{
+ const id=Number(req.params.id)
+ const user=users.find((user)=> user.id==id)
+  return res.send({status: "Pending"})
+})
